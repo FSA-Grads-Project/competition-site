1ab48d31a31ed81b1bbb43bcfa29778cf2f224d2
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2022-12-18 09:36:53 -0500

    add comments to submit.js

--- server/api/routes/submit.js
+++ server/api/routes/submit.js
@@ -7,40 +7,52 @@ const { spawn } = require("child_process")
 const vm = require('vm');
 
 router.post('/', async (req, res, next) => {
+
+  // code is user code
   let code = req.body.code
+  // create random filename
   let fileName = uuidv4() + '.js';
+  // create filepath in temp folder utilizing random filename 
   let filePath = path.join(__dirname, `/temp/${fileName}`);
 
       try {
+
+        // create new temp file containing user code
           fs.writeFileSync(filePath, code);
-
-          const consoleOutput = spawn("node", [filePath]);
-
+        
+        // execute file in vm sandbox + capture return value of last line of executable code and assign to contextOutput
           const sandbox = {}
           vm.createContext(sandbox);
-
           const contextOutput = vm.runInContext(code,sandbox)
-          let consoles = []
           let contextOutputArray = [contextOutput]
+
+        // start stream to execute file to capture consoles or error
+          const consoleOutput = spawn("node", [filePath]);
+          let consoles = []
+
+        // create result object to send back to client
           let result = { contextOutput: contextOutputArray, type: "", data: consoles | [], error: ""};
-      
+        
+        // listen for consoles, add to console array to add to result object
           consoleOutput.stdout.on('data', function (data) {
             consoles.push(data.toString().trim());
             result.type = "success";
             result.data = consoles
         });
 
+        // listen for errors, add to result object
           consoleOutput.stderr.on('data', function (error) {
             result.type = "error";
             result.error = error.toString();
         });
-
+        // listen for closing of stream, delete temp file + send result object to client
           consoleOutput.on('close', (code) => {
             fs.unlinkSync(filePath)
             res.json(result)
       });
 
     } catch(er){
+      // if error, delete temp file, send error to client
       fs.unlinkSync(filePath)
       res.json(String(er))
       next(er)
