73221698b4ef228d3ff974be9ab12cef5f6eadd6
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-01-27 10:19:31 -0500

    change port back to 3000

--- client/Components/Header.js
+++ client/Components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import Nav from "./Nav";
 import { Logo, Issue, Head, Hidden } from "../StyledComponents/AppStyles.tw";
