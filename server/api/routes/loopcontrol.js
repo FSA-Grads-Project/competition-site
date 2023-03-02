852141a1ba54c53e4588a8018090443aacc94c6f
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-03-02 09:34:22 -0500

    add fix for 'for loop' infinite loop issue in loopcontrol.js

--- server/api/routes/loopcontrol.js
+++ server/api/routes/loopcontrol.js
@@ -24,7 +24,30 @@ module.exports = function(babel) {
                     null
                  )
               );
-        }
+        },
+        ForStatement: function transformFor(path) {
+          let variableName = path.scope
+           .generateUidIdentifier("timer");
+          let declaration = t.declareVariable(variableName);
+          path.scope.parent.push(declaration);
+          let definition = t.assignmentExpression(
+             "=",
+             variableName,
+             t.callExpression(t.memberExpression(t.identifier("Date"), t.identifier("now")), [])
+          );
+          path.insertBefore(t.expressionStatement(definition));
+          const lhs = t.parenthesizedExpression(t.binaryExpression("+", variableName, t.NumericLiteral(5000)));
+          path
+             .get("body")
+             .pushContainer(
+                "body",
+                t.ifStatement(
+                   t.binaryExpression(">", t.callExpression(t.memberExpression(t.identifier("Date"), t.identifier("now")), []), lhs),
+                   t.throwStatement(t.stringLiteral("Execution Timedout")),
+                   null
+                )
+             );
+       },
      }
   };
 };
