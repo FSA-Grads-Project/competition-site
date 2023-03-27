640a44cfc22f84217cf7de6b013915aab7dbdddc
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-03-27 09:48:32 -0400

    add fix so that platform flag is dynamic dependent on developer's computer

--- script/seed.js
+++ script/seed.js
@@ -181,7 +181,6 @@ of the node where Sherman the cat is located.`,
     initialCode: `function findSherman(root) {
   // write your code here
 }
-
 // example of the binary-tree class - do not edit
 class binaryTree {
   constructor(number, sherman = false) {
@@ -203,7 +202,6 @@ branches of the tree.`,
 
   const test1 = await Test.create({
     test: `
-
 class Node {
   constructor(number, sherman = false) {
     this.number = number;
@@ -212,7 +210,6 @@ class Node {
     this.right = null; 
   }
 }
-
 class NodeQueue {
   constructor(value) {
     this.value = value;
@@ -220,7 +217,6 @@ class NodeQueue {
     this.next = null;
   }
 }
-
 class Queue {
   constructor() {
     this.head = null;
@@ -230,7 +226,6 @@ class Queue {
   enqueue(value) {
     this.length += 1;
     let newNode = new NodeQueue(value);
-
     if (this.tail) {
       this.tail.next = newNode;
       newNode.prev = this.tail;
@@ -245,7 +240,6 @@ class Queue {
       this.length -= 1;
       const dequeuedNode = this.head;
       this.head = this.head.next;
-
       if (this.head) {
         this.head.prev = null;
       } else {
@@ -256,27 +250,20 @@ class Queue {
     return undefined;
   }
 }
-
 function binaryTreeGenerator(N, nodeSherman) {
   let primaryQueue = new Queue(); 
-
   for (let i = 0; i < N; i++) {
     let node = new Node(i);
     if (i === nodeSherman - 1) node.sherman = true;
     primaryQueue.enqueue(node);
   }
-
   let secondaryQueue = new Queue();
-
   let root = primaryQueue.dequeue();
   secondaryQueue.enqueue(root);
-
   while (secondaryQueue.length > 0) {
     let p = secondaryQueue.dequeue();
-
     let pL = primaryQueue.dequeue();
     let pR = primaryQueue.dequeue();
-
     if (pL !== undefined) {
       secondaryQueue.enqueue(pL);
       p.left = pL;
@@ -288,9 +275,7 @@ function binaryTreeGenerator(N, nodeSherman) {
   }
   return root;
 }
-
 function runSubmission() {
-
   const root = binaryTreeGenerator(50000, 50000);
   let start = process.hrtime.bigint();
   let result = findSherman(root);
@@ -298,7 +283,6 @@ function runSubmission() {
   let resultTest = "";
   let resultMemory;
   let resultTime;
-
   if (result === 49999) {
     resultTest = 'test passed: sherman was at node 49999';
     resultMemory = process.memoryUsage().heapUsed;
@@ -308,7 +292,6 @@ function runSubmission() {
     resultMemory = 'None.'
     resultTime = 'None.'
   }
-
   return resultTest + ',' + 'resultTime: ' + resultTime + ',' + 'resultMemory: ' + resultMemory
 }
 `,
