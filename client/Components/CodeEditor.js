7ade7ea002f54f4a4bc84f7fe8f3f23795df7883
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-05-31 17:05:17 -0400

    refactor

--- client/Components/CodeEditor.js
+++ client/Components/CodeEditor.js
@@ -272,7 +272,7 @@ export const CodeEditor = ({ auth, solution, current }) => {
           </EditorButton>
         </ButtonWrapper>
       )}
-      { isEvaluating ? timeOutMessage : "" }
+      { isEvaluating && timeOutMessage }
       {solutionCompletedDate ? null : (
         <div id='output-container' className='text-darkFont'>
           <EditorAndOutputDiv id='editor-output' className='pb-0'>
